245c84ed226667df4248e71ace97d1865b7ad5ad
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-09-19 01:10:37 -0600

    basic interface Setup

--- app/src/main/java/com/example/pwdgenerator/MainActivity.java
+++ app/src/main/java/com/example/pwdgenerator/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.pwdgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Switch incUpperCase;
+    Switch incLowerCase;
+    Switch incNumbers;
+    Switch incSpecialSymbols;
+    Button generate;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        incLowerCase = (Switch) findViewById(R.id.incLowerCase);
+        incUpperCase = (Switch) findViewById(R.id.incUpperCase);
+        incNumbers = (Switch) findViewById(R.id.incNumbers);
+        incSpecialSymbols = (Switch) findViewById(R.id.incSpecialSymbol);
+        generate = (Button) findViewById(R.id.generate);
+        textView = (TextView) findViewById(R.id.textView);
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Do Whatever");
+            }
+        });
     }
 }
